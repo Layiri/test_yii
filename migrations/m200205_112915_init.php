95abd205f9744d3c5ed36913a7e61b67f85e5fae
Layiri <dev3.sofona@gmail.com>
2020-02-05 13:36:06 +0200

    installtion of yii2-basic

---
+++ migrations/m200205_112915_init.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200205_112915_init
+ */
+class m200205_112915_init extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+
+    }
+
+}
